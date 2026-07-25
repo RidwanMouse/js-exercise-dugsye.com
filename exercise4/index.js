c92aa5f5b14d78056cbdc2227b8334f37b252743
RidwanMouse <hajiridwan4028@gmail.com>
2026-07-25 09:58:23 +0300

    added exercise4

---
+++ exercise4/index.js
@@ -0,0 +1,36 @@
+
+// funtion declare
+function add(a,b) {
+    
+    return a + b;
+
+}
+
+
+let result = add( 45,6);
+let total = add(5,6);
+console.log(" question 1 function declaration")
+ console.log(result)
+ console.log(total)
+
+// function expretion
+
+ const greet = function (name) {
+    console.log("asalaama calaykum: " + name +"!")
+ }
+ console.log("question 2 function expresion");
+ greet("Ridwan");
+
+// arrow funtion
+ const iskudar = (a, b) =>  {
+
+    return a + b
+    
+ };
+ console.log("question 3 arrow funtion")
+ console.log(iskudar(10,20));
+ console.log(iskudar(40,50));
+ console.log(iskudar(70,20));
+ console.log(iskudar(100,20));
+ 
+ 
