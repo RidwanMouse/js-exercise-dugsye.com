9e224d5a7cb937714296e362d9148ec4fa09a728
RidwanMouse <hajiridwan4028@gmail.com>
2026-07-25 17:46:23 +0300

    added exercise7

---
+++ exercise7/main.js
@@ -0,0 +1,23 @@
+
+
+let colors = ["Red", "blue", "green", "yellow"]
+
+console.log(colors);
+console.log("after shift")
+colors.shift("")
+console.log(colors)
+
+console.log(colors);
+console.log("after unshift")
+colors.unshift("Red")
+console.log(colors)
+
+console.log(colors);
+console.log("after push")
+colors.push("green")
+console.log(colors)
+
+console.log(colors);
+console.log("after pop")
+colors.pop("")
+console.log(colors)
