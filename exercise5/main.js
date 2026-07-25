91f4ed2c0ff39d55f65a48a496f74af8033186c5
RidwanMouse <hajiridwan4028@gmail.com>
2026-07-25 10:13:14 +0300

    added exercis5

---
+++ exercise5/main.js
@@ -0,0 +1,11 @@
+
+
+const multiplication = (a,b) => {
+
+    return a * b;
+
+}
+
+console.log("qaybtan maadaamoon hore ugasoo shaqeeyay waxaa u badashay isku dhufashon maadaamoy ahayd isku dar")
+console.log(multiplication (4,5));
+console.log(multiplication (4,4));
