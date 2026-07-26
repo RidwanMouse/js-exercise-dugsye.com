5e9cc45fe2e5ceca0337d868be29f7c84f74e2a2
RidwanMouse <hajiridwan4028@gmail.com>
2026-07-26 10:34:29 +0300

    added exercise10

---
+++ exercise10/main.js
@@ -0,0 +1,9 @@
+
+
+let libarry = [
+    {title: "gafdiid", auth: "abdihakim abdilahi jibril", year: 2020},
+    {title: "Artificial intelegence", author: "timocacde", year: 2025}
+]
+
+console.log(libarry[0].title)
+console.log(libarry[1].author)
