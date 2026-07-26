652f6e74657a7615926a9d157f821f489bfafaba
RidwanMouse <hajiridwan4028@gmail.com>
2026-07-26 20:12:34 +0300

    added exercise17

---
+++ exercise17/main.js
@@ -0,0 +1,11 @@
+let tempre = 26;
+
+if (tempre <= 0) {
+  console.log("very cold");
+} else if (tempre <= 15) {
+  console.log("cold");
+} else if (tempre <= 25) {
+  console.log("warm");
+} else {
+  console.log("hot");
+}
