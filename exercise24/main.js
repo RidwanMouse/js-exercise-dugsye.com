800031d3c61a476fabcd89948758d512b5a656da
RidwanMouse <hajiridwan4028@gmail.com>
2026-08-01 11:19:22 +0300

    added aexercise 24

---
+++ exercise24/main.js
@@ -0,0 +1,8 @@
+ function calculatedArea(width, hieght = width){
+    return width * hieght;
+ }
+
+ console.log(calculatedArea(5,25));
+
+ console.log('defualt parameter')
+ console.log(calculatedArea(10));
